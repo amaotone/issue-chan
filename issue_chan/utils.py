0191b23a1049f99330307c9d0ff1e455278d68b5
amaotone <amane.suzu@gmail.com>
2019-04-24 14:16:01 +0900

    ひとまず完成

--- issue_chan/utils.py
+++ issue_chan/utils.py
@@ -1,17 +1,33 @@
+import json
 import logging
-import sys
+import os
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     logger.propagate = False
 
     sh = logging.StreamHandler()
-    sh.setLevel(logging.INFO)
+    sh.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] %(message)s ")
     sh.setFormatter(formatter)
 
     logger.addHandler(sh)
     return logger
+
+
+def load_mapping(path):
+    # なかったら空で作成
+    if not os.path.exists(path):
+        update_mapping(path, dict())
+
+    with open(path) as f:
+        mapping = json.load(f)
+    return mapping
+
+
+def update_mapping(path, mapping):
+    with open(path, 'w') as f:
+        json.dump(mapping, f)
--- main.py
+++ main.py
@@ -3,30 +3,65 @@ import time
 
 from issue_chan.issue import IssueManager
 from issue_chan.slack import SlackManager
-from issue_chan.utils import get_logger
+from issue_chan.utils import get_logger, load_mapping, update_mapping
+
+MAPPING_PATH = 'config/mapping.json'
 
 
 def check_reaction(event):
     """issueスタンプがついていたらissueを立て、slackにURLを通知する"""
+
     event = slack.check_reaction(event, 'issue')
     if event is not None:
         channel, ts = event['item']['channel'], event['item']['ts']
+
+        # 紐付いているリポジトリがなければ使い方を表示
+        if channel not in mapping:
+            send_usage(channel)
+            return
+
         content = slack.get_content(channel, ts)
         link = slack.get_permalink(channel, ts)
 
         issue_body = f"{content}\nlink: {link}"
-        iss, created = issue.create('amane-suzuki/sandbox', content, issue_body)
+        iss, created = issue.create(mapping[channel], content, issue_body)
 
         if created:
-            slack_message = f"issueを作成しました！\n{iss.html_url}"
+            message = f"issueを作成しました！\n" \
+                f"→ {iss.html_url}"
         else:
-            slack_message = f"同じissueが既にありますよ！\n{iss.html_url}"
-        slack.send_message(channel, slack_message)
+            message = f"同じissueが既にありますよ！\n" \
+                f"→ {iss.html_url}"
+        slack.send_message(channel, message)
 
 
-def check_reply(event):
-    """replyで設定項目が飛んできたら設定し、そうでなければusageを提示"""
-    pass
+def check_command(event):
+    """設定項目が発言されたら設定し、そうでなければ使い方を提示"""
+
+    prefixes = ('issue-chan', 'issue_chan')
+    if 'type' in event and event['type'] == 'message':
+        texts = event['text'].split()
+        if texts[0] not in prefixes:
+            return
+
+        channel = event['channel']
+        if len(texts) >= 3 and texts[1] == 'set':
+            # リポジトリのマッピングを変更
+            repo = texts[2]
+            mapping[channel] = repo
+            update_mapping(MAPPING_PATH, mapping)
+            message = f"このチャンネルと `repo` を紐付けました！"
+            slack.send_message(channel, message)
+            logger.info('link channel=%s -> repository=%s', channel, repo)
+        else:
+            send_usage(channel)
+
+
+def send_usage(channel):
+    message = f"つかいかた\n" \
+        f"> 1. `issue-chan set hoge/fuga` でリポジトリをこのチャンネルと紐付けます\n" \
+        f"> 2. issue化したいメッセージに `:issue:` :issue: スタンプをつければ、紐付いているリポジトリにissueを立てます"
+    slack.send_message(channel, message)
 
 
 def main():
@@ -35,7 +70,8 @@ def main():
     while slack.client.server.connected:
         events = slack.client.rtm_read()
         for event in events:
-            check_reply(event)
+            logger.debug(event)
+            check_command(event)
             check_reaction(event)
         time.sleep(1)
 
@@ -48,4 +84,5 @@ if __name__ == '__main__':
 
     slack = SlackManager(config['slack_api_token'])
     issue = IssueManager(config['github_hostname'], config['github_app_id'], config['github_installation_id'])
+    mapping = load_mapping(MAPPING_PATH)
     main()
